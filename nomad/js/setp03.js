f286890306f77cf0fbab2a00b30f3b6e4724dd88
KyeongHwanSong <sting7410@naver.com>
2021-07-18 17:40:20 +0900

    go

--- nomad/js/setp03.js
+++ nomad/js/setp03.js
@@ -7,6 +7,11 @@ const usestrict = new UseStrict;
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
 const loginButton = loginForm.querySelector('button');
+const greeting = document.querySelector('#greeting');
+
+// 반복되는 스트링은 미리 지정해서 사용 , 대문자로 
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'userName';
 
 function handleLoginClick() {
     const username = loginInput.value;
@@ -22,4 +27,34 @@ function handleLoginClick() {
     
 }
 
-loginButton.addEventListener('click', handleLoginClick);
+// loginButton.addEventListener('click', handleLoginClick);
+
+function onLoginSubmit(e) {
+    e.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGreeeting(userName);
+}
+
+const link = document.querySelector('a');
+function handleLink(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
+function paintGreeeting(userName) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${userName}`
+}
+
+link.addEventListener('click', handleLink);
+
+const saveUserName = localStorage.getItem(USERNAME_KEY);
+console.log(saveUserName);
+if(saveUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    paintGreeeting(saveUserName);
+}
